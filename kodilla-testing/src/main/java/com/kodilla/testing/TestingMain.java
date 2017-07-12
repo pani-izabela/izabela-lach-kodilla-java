f6c12d17c3a15e1deabf4c2c2b0e3fb501fa8feb
“pani-izabela <izabela.lach86@gmail.com>
2017-07-12 11:22:50 +0200

    Commit calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //Test dla klasy calculator
+        Calculator calculator = new Calculator();
+        int difference = calculator.subtract(8, 4);
+        if (difference == 4){
+            System.out.println("Odejmowanie działa! OK");
+        } else {
+            System.out.println("Odejmowanie nie działa! Error");
+        }
+
+        int total = calculator.add(8, 4);
+        if (total == 12){
+            System.out.println("Dodawanie działa! OK");
+        } else {
+            System.out.println("Dodawanie nie działa! Error");
+        }
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
@@ -0,0 +1,16 @@
+package com.kodilla.testing.user;
+
+/**
+ * Created by 430 on 2017-07-12.
+ */
+public class SimpleUser {
+    private String username;
+
+    public SimpleUser(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
