349dafbfc2eb3bff7dd730927963849a5aa3c19b
“pani-izabela <izabela.lach86@gmail.com>
2017-07-18 10:37:44 +0200

    6.5-Mockito wyswietla sie na czerwono

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.forum.statistics;
+
+
+public class ForumStatistics {
+
+    public int calculateAdvStatistics(Statistics statistics) {
+
+    }
+
+    /*public String ShowStatistics(){
+
+    }*/
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
@@ -0,0 +1,11 @@
+package com.kodilla.testing.forum.statistics;
+
+/**
+ * Created by 430 on 2017-07-18.
+ */
+public class ForumStatisticsTestSuite {
+
+    public int calculateAdvStatistics(Statistics statistics){
+
+    }
+}
