9c8393cf5acfa90c9c21b3a4babf327589cee6dc
“pani-izabela <izabela.lach86@gmail.com>
2017-07-19 09:19:27 +0200

    6.5 - Zadanie - pierwsze przesłanie

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
@@ -2,19 +2,51 @@ package com.kodilla.testing.forum.statistics;
 
 
 public class ForumStatistics {
-    int quantityUsers = 0;
-    int quantityPosts = 0;
-    int quantityComments = 0;
-    int avarageQuantityPostsUser = 0;
-    int avarageQuantityCommentsUser = 0;
-    int avarageQuantityCommentsPost = 0;
+    int quantityUsers;
+    int quantityPosts;
+    int quantityComments;
+    double avarageQuantityPostsUser;
+    double avarageQuantityCommentsUser;
+    double avarageQuantityCommentsPost;
 
-    public void calculateAdvStatistics(Statistics statistics) {
-
-
+    public ForumStatistics() {
     }
 
-    /*public String ShowStatistics(){
+    public int getQuantityUsers() {
+        return quantityUsers;
+    }
 
-    }*/
+    public int getQuantityPosts() {
+        return quantityPosts;
+    }
+
+    public int getQuantityComments() {
+        return quantityComments;
+    }
+
+    public double getAvarageQuantityPostsUser() {
+        return avarageQuantityPostsUser;
+    }
+
+    public double getAvarageQuantityCommentsUser() {
+        return avarageQuantityCommentsUser;
+    }
+
+    public double getAvarageQuantityCommentsPost() {
+        return avarageQuantityCommentsPost;
+    }
+
+    public void calculateAdvStatistics(Statistics statistics) {
+        int quantityUsers = statistics.usersNames().size();
+        int quantityPosts = statistics.postsCount();
+        int quantityComments = statistics.commentsCount();
+        double avarageQuantityPostsUser = (statistics.postsCount() / statistics.usersNames().size());
+        double avarageQuantityCommentsUser = (statistics.commentsCount() / statistics.usersNames().size());
+        double avarageQuantityCommentsPost = (statistics.commentsCount() / statistics.postsCount());
+    }
+
+    public String ShowStatistics(){
+        return null;
+    }
+
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
@@ -1,11 +1,35 @@
 package com.kodilla.testing.forum.statistics;
 
-/**
- * Created by 430 on 2017-07-18.
- */
-public class ForumStatisticsTestSuite {
+import com.kodilla.testing.forum.statistics.ForumStatistics;
+import com.kodilla.testing.forum.statistics.Statistics;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ForumStatisticsTestSuite {
+    @Test
     public void testCalculateAdvStatistics(Statistics statistics){
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        List<String> listNamesUsers = new ArrayList<String>();
+        listNamesUsers.add("Jurek");
+        listNamesUsers.add("Marek");
+        listNamesUsers.add("Felek");
+        listNamesUsers.add("Arek");
+        listNamesUsers.add("Ola");
+        when(statisticsMock.usersNames()).thenReturn(listNamesUsers);
+
+        ForumStatistics forumStatistics = new ForumStatistics();
+
+        //When
+
+
+        //Then
 
     }
 }
