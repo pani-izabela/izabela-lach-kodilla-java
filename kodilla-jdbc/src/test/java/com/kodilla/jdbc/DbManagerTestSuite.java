a4444cea81a9c07b98383dcb36c6149591089612
“pani-izabela <izabela.lach86@gmail.com>
2017-08-28 23:48:51 +0200

    12.6-cały modył

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -2,10 +2,7 @@ package com.kodilla.jdbc;
 
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class DbManagerTestSuite {
     @Test
@@ -38,30 +35,5 @@ public class DbManagerTestSuite {
         rs.close();
         statement.close();
         Assert.assertEquals(5, counter);
-    }
-
-    @Test
-    public void testSelectUsersAndPosts() throws SQLException {
-        //Given
-        DbManager dbManager = DbManager.getInstance();
-        //When
-        String sqlQueryMain = "SELECT U.FIRSTNAME, U.LASTNAME, COUNT(*) AS POSTS_NUMBER\n" +
-                "FROM POSTS P, USERS U\n" +
-                "WHERE  U.ID = P.USER_ID\n" +
-                "GROUP BY P.USER_ID\n" +
-                "HAVING COUNT(*) >= 2";
-        Statement statement = dbManager.getConnection().createStatement();
-        ResultSet rs = statement.executeQuery(sqlQueryMain);
-
-        //Then
-        int counter = 0;
-        while(rs.next()) {
-            System.out.println(rs.getString("FIRSTNAME") + ", " +
-                    rs.getString("LASTNAME"));
-            counter++;
-        }
-        rs.close();
-        statement.close();
-        Assert.assertEquals(2, counter);
     }
 }
