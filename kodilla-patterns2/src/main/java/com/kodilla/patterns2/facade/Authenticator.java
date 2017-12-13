351a81d5c600ee1da6114951414f69bc4ec83563
“pani-izabela <izabela.lach86@gmail.com>
2017-12-13 23:36:21 +0100

    20.3 - submodul

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/Authenticator.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/Authenticator.java
@@ -1,7 +1,9 @@
 package com.kodilla.patterns2.facade;
 
-import java.util.Random;
+import org.springframework.stereotype.Service;
 
+import java.util.Random;
+@Service
 public class Authenticator {
     public boolean isAuthenticated(Long userId){
         Random random = new Random();
